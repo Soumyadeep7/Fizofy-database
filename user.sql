98fed6e09b7ddf266bdf3244942fc5a7a69a8129
Soumyadeep7 <111475097+Soumyadeep7@users.noreply.github.com>
2025-10-15 13:10:52 +0530

    Add files via upload

---
+++ user.sql
@@ -0,0 +1,16 @@
+psql:/sandbox-data/tasks/253742297/submission.txt:6: ERROR:  permission denied to create extension  cube 
+HINT:  Must be superuser to create this extension.
+psql:/sandbox-data/tasks/253742297/submission.txt:7: ERROR:  required extension  cube  is not installed
+HINT:  Use CREATE EXTENSION ... CASCADE to install required extensions too.
+psql:/sandbox-data/tasks/253742297/submission.txt:10: NOTICE:  type  user_role  does not exist, skipping
+psql:/sandbox-data/tasks/253742297/submission.txt:11: NOTICE:  type  gender_type  does not exist, skipping
+psql:/sandbox-data/tasks/253742297/submission.txt:12: NOTICE:  type  appointment_status  does not exist, skipping
+psql:/sandbox-data/tasks/253742297/submission.txt:13: NOTICE:  type  payment_method  does not exist, skipping
+psql:/sandbox-data/tasks/253742297/submission.txt:14: NOTICE:  type  payment_status  does not exist, skipping
+psql:/sandbox-data/tasks/253742297/submission.txt:15: NOTICE:  type  document_type  does not exist, skipping
+psql:/sandbox-data/tasks/253742297/submission.txt:16: NOTICE:  type  notification_type  does not exist, skipping
+psql:/sandbox-data/tasks/253742297/submission.txt:61: ERROR:  syntax error at or near  FUNCTION 
+LINE 4: EXECUTE FUNCTION generate_user_code();
+                ^
+psql:/sandbox-data/tasks/253742297/submission.txt:166: ERROR:  syntax error at or near  FUNCTION 
+LINE 4: EXECUTE FUNCTION generate_appointment_code();
